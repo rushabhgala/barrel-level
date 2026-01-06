75466f996ff6e0aac7f111bf2a7cc8b7c3a310dd
rushabhgala <rushabh@rushabh.space>
2026-01-06 11:42:53 +0000

    Fixed an error in the for loop and improved the wording of the output statements

--- apps/barrel_cli.cpp
+++ apps/barrel_cli.cpp
@@ -6,12 +6,12 @@
 #include <string>
 
 static double get_arg(int argc, char** argv, const std::string& name) {
-    for (int i = 1; i+1 < argc; ++1) {
+    for (int i=1; i+1 < argc; ++i) {
         if (argv[i] == name) {
             return std::stod(argv[i+1]);
         }
     }
-    throw std:invalid_argument("Missing argument:" + name);
+    throw std::invalid_argument("Missing argument: " + name);
 }
 
 int main(int argc, char** argv) {
@@ -24,11 +24,11 @@ int main(int argc, char** argv) {
         barrel::VolumeSolver solver(b);
 
         const double h = solver.height_for_removed_volume(vol_removed);
-        std::cout << "height = " << h << "\n";
+        std::cout << "height of filled liquid = " << h << "\n";
         return 0;
     } catch (const std::exception& e) {
-        std:cerr << "Error: " << e.what() << "\n";
-        std:cerr << "Usage: barrel_cli --radius R --length L --removed_vol V_removed \n"
+        std::cerr << "Error: " << e.what() << "\n";
+        std::cerr << "Usage: barrel_cli --radius R --length L --removed_vol V_removed \n";
         return 1;
     }
 }
